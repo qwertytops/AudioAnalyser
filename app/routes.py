36499100e9f1db5a3776ee8c9fe646c9992282df
LiamvdM <lvandermoezel@gmail.com>
2025-04-17 14:00:51 +0800

    integrated files from main branch into flask structure

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,8 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
+    return render_template('introductoryView.html')
+
+@app.route('/analysis')
+def analysis():
     return render_template('analysisView.html')
