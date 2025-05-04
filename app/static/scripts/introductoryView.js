1a8cb4bdfb2754ec27fcc11434d1ee44258d72cd
Milan Scekic <milanscekic@hotmail.com>
2025-05-04 16:30:11 +0800

    Removed redundant code from introductoryView files

--- app/static/scripts/introductoryView.js
+++ app/static/scripts/introductoryView.js
@@ -1,49 +1,9 @@
 function showLogInForm() {
-    document.getElementById('logInForm').style.display = 'block';  
-    document.getElementById('signUpForm').style.display = 'none';  
-}
-function showSignUpForm() {
-    document.getElementById('signUpForm').style.display = 'block';   
-    document.getElementById('logInForm').style.display = 'none';
-} 
-
-
-function submitSignUp() {
-    const username = document.getElementById("signUpUsername").value;
-    const password = document.getElementById("signUpPassword").value;
-    const email = document.getElementById("signUpEmail").value;
-
-    if (username === "" || password === "" || email === "") {
-        alert("Please fill in all fields.");
-        return;
-    }
-
-    const userData = {
-        username: username,
-        password: password,
-        email: email
-    };
-
-    //localStorage.setItem(username, JSON.stringify(userData)); <- this is what we need to do to save the data in the DB
-    alert("Sign up successful! jk it doesnt work yet 💀");
+    document.getElementById('logInForm').style.display = 'block';   
 }
 
 function submitLogIn() {
-    const email = document.getElementById("logInEmail").value;
-    const password = document.getElementById("logInPassword").value;
-
-    if (email === "" || password === "") {
-        alert("Please fill in all fields.");
-        return;
-    }
-
-    const userData = JSON.parse(localStorage.getItem(email)); // <- this is what we need to do to get the data from the DB
-    if (userData && userData.password === password) {
-        alert("Login successful!");
-        // Redirect them to the main page or something here
-    } else {
-        alert("Invalid username or password. the DB doesnt exist yet");
-    }
+    // NEED TO IMPLEMENT THIS
 }
 
 function toggleTheme() {
